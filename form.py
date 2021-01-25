ca5ffd72a44c8ba43a2f3ba944e6f5a75163e921
Ian Rees <ian.rees@gmail.com>
2021-01-25 19:46:29 +1300

    A bit more refactoring
    The idea is to separate the different tasks in to their own functions.
    Some of them are a bit shorter than they might need to be, however it's
    nice to have just a few specific places that, for instance, touch the
    database.

--- form.py
+++ form.py
@@ -21,11 +21,11 @@ def list_readings():
     plt.show()
 
 def ketone_level():
-    "Returns the ketone level from the GUI"
+    "Returns the ketone level from the GUI as float"
     return float(ket.get())
 
 def glucose_level():
-    "Returns the glucose level in mmol/L from the GUI"
+    "Returns the glucose level in mmol/L from the GUI as float"
     raw = float(glu.get())
 
     # If user selected a mg/dl checkbox, convert glucose to mmol/L
@@ -34,41 +34,48 @@ def glucose_level():
     else:
         return raw
 
-def gki():
-    "Gets user input from database, runs the GKI calculation, stores result"
-    k = ketone_level()
-    g = glucose_level()
+def display_gki(glucose, keytone):
+    "Updates GUI with GKI calculation results"
+    gki = glucose/ketone
 
-    d = datetime.datetime.now()
-
-    u = 1 # Hard coded user_ID for readings table this will get replaced when I add reg/login 
-    
-    results = g/k
-
-    if results <1:
+    if gki <1:
         lvl = "You're in the highest therapeutic level ketosis."
-    elif results <=3:
+    elif gki <=3:
         lvl = "You're in a high therapeutic level of ketosis."
-    elif results <=6:
+    elif gki <=6:
         lvl = "You're in a moderate level of ketosis."
-    elif results <=9:
+    elif gki <=9:
         lvl = "You're in a low level of ketosis."
     else:
         lvl = "You are not in ketosis"
-    sql_command = "INSERT INTO readings (user_id, ketones, glucose, rdate) VALUES(%s, %s, %s, %s)"
-    values = (u, k, g, d)
-    db.execute(sql_command,values)
-    mydb.commit()
+
     label_gki = Label(screen, text="Your GKI is: ")
     label_gki.place(x=12, y=240)
-    label_round = Label(screen, text=round(results,2)) # Rounds results to two decimal places
+    label_round = Label(screen, text=round(gki,2)) # Rounds results to two decimal places
     label_round.place(x=90, y=240) 
     lable_lvl = Label(screen, text=lvl)
     lable_lvl.place(x=30, y=280)
+
+def store_readings(user_id, ketone, glucose):
+    "Stores user-supplied data in the database"
+
+    db.execute("INSERT INTO readings (user_id, ketones, glucose, rdate) VALUES(%s, %s, %s, %s)",
+        (user_id, ketone, glucose, datetime.datetime.now()))
+    mydb.commit()
+
+def gki_clicked():
+    "Gets user input from database, runs the GKI calculation, stores result"
+    ketone = ketone_level()
+    glucose = glucose_level()
+
+    user_id = 1 # This will get replaced when I add reg/login 
+
+    store_readings(user_id, ketone, glucose)
+
+    display_gki(glucose, keytone)
+
     ketones_entry.delete(0,END)
     glucose_entry.delete(0,END)
-
-
 
 
 screen = Tk()
@@ -99,7 +106,7 @@ glucose_entry.place(x=120, y=120)
 # Creates buttons.
 mgdl_checkbox = Checkbutton(screen, text= "Are you using mg/dL?", variable=mgdl)
 mgdl_checkbox.place(x= 120, y= 150)
-calculate_button = Button(screen, text = "Calculate", width= "7", bg = "grey", command = gki)
+calculate_button = Button(screen, text = "Calculate", width= "7", bg = "grey", command = gki_clicked)
 calculate_button.place(x= 150, y=180)
 list_readings_button = Button(screen, text="Pull Readings", bg= "grey", command=list_readings)
 list_readings_button.place(x= 250, y= 180)
